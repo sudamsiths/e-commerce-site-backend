70cd721fc548346caf38cb4f4bcf67087fe230da
sudamsiths <sudamsiths@gmail.com>
2026-01-31 11:39:46 +0530

    initial commit

---
+++ src/main/java/ecom/study/ECommerceSiteApplication.java
@@ -0,0 +1,13 @@
+package ecom.study;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSiteApplication.class, args);
+	}
+}
